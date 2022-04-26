9c855da16c5c7a4e548748d44311d69b7611d18f
gmarzo <garrett.marzo@gmail.com>
2022-04-25 18:50:16 -0700

    12/15 working

--- regex_practice.js
+++ regex_practice.js
@@ -10,12 +10,12 @@ const regexes = {
   sevenThroughThirtyOne: /^[7-9]$|^[12]\d$|^3[01]$/,
   mLComment: /^\(\*+\s?\S*\s?\*+\)$/,
   //   notFileForFirstNoLookAround: _________,
-  notFileForFirstWithLookAround: /(?!file|for|first)(.*)/,
+  notFileForFirstWithLookAround: /^(?!^file$|^for$|^first$)(.*)$/,
   cOctal: /^0[0-7]*$/,
   restrictedFloatingPoint: /^\d+(\.\d*)?((E|e)(\+|\-)?\d{1,3})?$/,
   palindrome2358:
     /^([^\p{C}\s])\1$|^([^\p{C}\s])([^\p{C}\s])\2$|^([^\p{C}\s])([^\p{C}\s])([^\p{C}\s])\5\4$|^([^\p{C}\s])([^\p{C}\s])([^\p{C}\s])([^\p{C}\s])\10\9\8\7$/u,
-  noNegativeIntLits: /^(.(?!\b\-))+$/,
+  noNegativeIntLits: /^(?!(\b\-.)+).+$/,
   repeated: /^([a-z]*)\1$/,
 };
 
