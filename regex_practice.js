59613630632a353a5c294ceeccd88e6c12d35678
gmarzo <garrett.marzo@gmail.com>
2022-04-26 17:40:46 -0700

    13/15, need nonNegativeInt and NoLookAround

--- regex_practice.js
+++ regex_practice.js
@@ -4,18 +4,19 @@ const regexes = {
   visa: /^4(\d{15}|\d{12})$/,
   masterCard:
     /^(5[1-5]\d{14})$|^(2((2(2[1-9]|[3-9]\d))|[3-6]\d{2}|7([01]\d|20)))\d{12}$/,
-  //   adaFloat: _________,
+  adaFloat:
+    /^((\d(_\d)?)+(\.(\d((_\d)+)?)+)?(([Ee]\+?(\d(_\d)?)+)|([Ee]\-(\d(_\d)?)+))?)$|^(((\d(_\d)?)+)#(([\dA-Fa-f]((_[\dA-Fa-f])+)?)+)(\.([\dA-Fa-f](_[\dA-Fa-f])?)+)?#((E\+?(\d(_\d)?)+)|(E\-(\d(_\d)?)+))?)$/,
   notThreeEndingInOO: /^(\p{L}(?![oO]{2}\b)\p{L}*)?$/u,
   divisibleBy32: /^0{1,4}$|^[01]*(?<=0{5,})$/,
   sevenThroughThirtyOne: /^[7-9]$|^[12]\d$|^3[01]$/,
   mLComment: /^\(\*+\s?\S*\s?\*+\)$/,
-  //   notFileForFirstNoLookAround: _________,
+  notFileForFirstNoLookAround: /.*/,
   notFileForFirstWithLookAround: /^(?!^file$|^for$|^first$)(.*)$/,
   cOctal: /^0[0-7]*$/,
   restrictedFloatingPoint: /^\d+(\.\d*)?((E|e)(\+|\-)?\d{1,3})?$/,
   palindrome2358:
     /^([^\p{C}\s])\1$|^([^\p{C}\s])([^\p{C}\s])\2$|^([^\p{C}\s])([^\p{C}\s])([^\p{C}\s])\5\4$|^([^\p{C}\s])([^\p{C}\s])([^\p{C}\s])([^\p{C}\s])\10\9\8\7$/u,
-  noNegativeIntLits: /^(?!(\b\-.)+).+$/,
+  noNegativeIntLits: /^((?<!\b\-).)+$/,
   repeated: /^([a-z]*)\1$/,
 };
 
